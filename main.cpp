64d4e5cb6672c918a678fc7c6e74dbfafd60c001
Joe2174 <jbarron36@student.rccd.edu>
2026-09-10 12:26:02 -0700

    Compute Calculator with Doubles.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main() {
   double sum = 0.0;
   double difference = 0.0;
   double product = 0.0;
+  double ratio = 0.0;
 
   // User input for two numbers
   cout << "First number: ";
@@ -23,11 +24,13 @@ int main() {
   sum = a + b;
   difference = a - b;
   product = a * b;
+  ratio = a / b;
 
   // Print the labeled results
   cout << "Sum: " << sum << "\n";
   cout << "Difference: " << difference << "\n";
   cout << "Product: " << product << "\n";
+  cout << "Ratio: " << ratio << "\n";
 
   return 0;
 }
